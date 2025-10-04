5d8fc2c455b73d47b43efa0909c0c316f0ffe8d2
Ivan Novikov <novivan05@10.215.166.154-vpn.dhcp.yndx.net>
2025-10-04 03:05:01 +0300

    добавил тестов по issue

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -4,7 +4,6 @@ from app.main import app
 
 client = TestClient(app)
 
-
 def test_empty_users():
     r = client.get("/list_users")
     assert r.status_code == 200
@@ -14,4 +13,28 @@ def test_empty_users():
 
 def test_adding_user():
     r = client.post("/add_user", params={"user_name": "aaaa"})
+    assert r.status_code == 200
+
+
+def test_empty_features():
+    r = client.get("/list_features")
+    assert r.status_code == 200
+    body = r.json()
+    assert len(body) == 0
+
+
+def test_adding_user():
+    r = client.post("/add_feature", params={"feature_title": "bbbb", "feature_description": "cccc"})
+    assert r.status_code == 200
+
+
+def test_empty_votes():
+    r = client.get("/list_votes")
+    assert r.status_code == 200
+    body = r.json()
+    assert len(body) == 0
+
+
+def test_adding_vote():
+    r = client.post("/add_feature", params={"feature_id": "1", "user_id": "1"})
     assert r.status_code == 200
