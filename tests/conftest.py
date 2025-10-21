cbb10188c3ce57bcd993497ca33146ebc42792c7
Ivan Novikov <novivan05@MacBook-Pro.local>
2025-10-21 12:32:48 +0300

    added cleint for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,16 @@
 import sys
 from pathlib import Path
 
-ROOT = Path(__file__).resolve().parents[1]  # корень репозитория
+import pytest
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+ROOT = Path(__file__).resolve().parents[1]
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
